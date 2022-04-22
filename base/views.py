a0a0cae112f815c42560c629abae6b9df56689fa
sankar050 <jioj2138@gmail.com>
2022-04-22 12:38:05 +0530

    data is resricted to specific user only views his own tasks

--- base/views.py
+++ base/views.py
@@ -23,14 +23,23 @@ class TaskList(LoginRequiredMixin,ListView):
     model = Task
     context_object_name = "tasks"
     # template_name = "task_list.html" by defalut listview search for template location is this templates/base/task_list.html
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count']= context['tasks'].filter(complete=False).count()
+        return context
 class DetailsView(LoginRequiredMixin,DetailView):
     model = Task
     template_name = "base/task.html"
     context_object_name = "task"
 class TaskCreate(LoginRequiredMixin,CreateView):
     model = Task
-    fields = "__all__"
+    fields = ['title','description','complete']
     success_url = reverse_lazy("tasks")
+    def form_valid(self,form):
+        form.instance.user = self.request.user
+        return super(TaskCreate,self).form_valid(form)
+    
 class TaskUpdate(LoginRequiredMixin,UpdateView):
     model = Task
     fields = "__all__"
