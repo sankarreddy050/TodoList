3dced1eef7fa6291ab70e17366dcc298a8947d2c
sankar050 <jioj2138@gmail.com>
2022-04-22 11:05:12 +0530

    pages restricting using LoginRequiredMixin

--- base/views.py
+++ base/views.py
@@ -5,6 +5,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.urls import reverse_lazy
 from .models import Task
@@ -18,23 +19,23 @@ class CustomLoginView(LoginView):
         return reverse_lazy("tasks")
 class Logout(LogoutView):
     next_page = "login"
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin,ListView):
     model = Task
     context_object_name = "tasks"
     # template_name = "task_list.html" by defalut listview search for template location is this templates/base/task_list.html
-class DetailsView(DetailView):
+class DetailsView(LoginRequiredMixin,DetailView):
     model = Task
     template_name = "base/task.html"
     context_object_name = "task"
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin,CreateView):
     model = Task
     fields = "__all__"
     success_url = reverse_lazy("tasks")
-class TaskUpdate(UpdateView):
+class TaskUpdate(LoginRequiredMixin,UpdateView):
     model = Task
     fields = "__all__"
     success_url = reverse_lazy("tasks")
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin,DeleteView):
     model =Task
     context_object_name = "task"
     success_url = reverse_lazy("tasks")   
